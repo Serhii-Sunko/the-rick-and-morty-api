5b38a7707c277b410c440f72b0eaa9f57857b185
Serhii Sunko <serhii@garrabridge.com>
2025-08-06 12:30:58 +0100

    preparation 0.0.1

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { store } from './redux/store';
 import { ThemeProvider } from 'styled-components';
 import theme from 'Utils/Theme';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from 'redux/store';
+import { persistor} from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
