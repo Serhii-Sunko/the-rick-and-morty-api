9d3951d2a81c17d75b5c75a6c8a679c161c430c8
Serhii Sunko <serhii@garrabridge.com>
2025-08-06 12:25:20 +0100

    preparation 0.0.1

---
+++ src/Utils/Theme.js
@@ -0,0 +1,100 @@
+export const theme = {
+    colors: {
+    white: '#ffffff',
+    black: '#000000',
+
+    accent: '#40de10',
+    primaryBackground: '',
+    secondaryBackground: '',
+
+    text: {
+      sectionTitle: '#FFAC1C',
+      primaryText: '#202c2d;',
+      secondaryText: '',
+      accent: '#2ee59d',
+    },
+
+    button: {
+      primaryBtnBackground: '#ffffff',
+      secondaryBtnBackground: '#de7676',
+      accentBtnBackground: '#2ee59d',
+      inputBorder: '#2ee59d66',
+      btnShadow: '#2ee59d66',
+      text: '#40de10',
+
+      primaryBtn: '',
+    },
+
+    form: {
+      formBorder: ' teal',
+      formTitle: '#212121',
+      formText: '#414141',
+      formPlaceholder: '',
+    },
+
+    searchInput: '',
+  },
+
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024],
+
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'inherit',
+    monospace: 'Menlo, monospace',
+  },
+
+  textTransform: {
+    cap: 'capitalize',
+    lowCase: 'lowercase',
+    upCase: 'uppercase',
+  },
+
+  fontSizes: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    l: '20px',
+    xl: '24px',
+    xxl: '32px',
+  },
+
+  fontWeights: {
+    thin: 100,
+    extraLight: 200,
+    light: 300,
+    normal: 400,
+    medium: 500,
+    semiBold: 600,
+    bold: 700,
+    extraBold: 800,
+  },
+
+  letterSpacing: {
+    s: '0.01rem',
+    m: '0.03rem',
+    l: '0.05rem',
+  },
+
+  lineHeights: {
+    body: 1.5,
+    form: 1.75,
+    sectionTitle: 1.75,
+    text: 1.25,
+  },
+
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+
+  radii: {
+    none: '0',
+    sm: '4px',
+    md: '8px',
+    lg: '16px',
+    xl: '30px',
+    round: '50%',
+  },
+};
+
+export default theme;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
+import theme from 'Utils/Theme';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter basename="the-rick-and-morty-api">
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
